43124266c792bd14644de2cab8edbc86d5816c19
Csaba Sipos <siposcsaba89@live.com>
2016-12-18 21:28:58 +0100

    android assimp build

--- build_android.sh
+++ build_android.sh
@@ -10,13 +10,18 @@ fi
 mkdir -p sources
 cd sources
 
-for proj in freetype
+assimp_FLAGS="-DASSIMP_BUILD_ASSIMP_TOOLS=0"
+
+for proj in freetype assimp
 do
 	if cd $proj 2> /dev/null; then git pull; cd ..; else git clone https://github.com/siposcsaba89/$proj.git; fi
 	cd $proj
+    opts=$proj"_FLAGS"
+    echo "$proj options are: ${!opts}"
+	echo ${!opts}
 	mkdir -p build_$1
 	cd build_$1
-	cmake .. -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=../../../cmake/toolchain/android.toolchain.cmake -DCMAKE_SYSTEM_NAME=windows -DCMAKE_DEBUG_POSTFIX=_d -DCMAKE_INSTALL_PREFIX=$PWD/../../../install/$1/ -DBUILD_SHARED=0 -DCMAKE_MAKE_PROGRAM="$ANDROID_NDK/prebuilt/windows-x86_64/bin/make" -DANDROID_STL=gnustl_static -DANDROID_ABI=$1 -DANDROID_STL=gnustl_static -DCMAKE_BUILD_TYPE=Debug
+	cmake .. -G "MinGW Makefiles" -DCMAKE_TOOLCHAIN_FILE=../../../cmake/toolchain/android.toolchain.cmake -DCMAKE_SYSTEM_NAME=windows -DCMAKE_DEBUG_POSTFIX=_d -DCMAKE_INSTALL_PREFIX=$PWD/../../../install/$1/ -DBUILD_SHARED=0 -DCMAKE_MAKE_PROGRAM="$ANDROID_NDK/prebuilt/windows-x86_64/bin/make" -DANDROID_STL=gnustl_static -DANDROID_ABI=$1 -DANDROID_STL=gnustl_static -DCMAKE_BUILD_TYPE=Debug ${!opts}
 	MAKEFLAGS=-j4 cmake --build . --target install
 	cmake .. -DCMAKE_BUILD_TYPE=Release
 	MAKEFLAGS=-j4 cmake --build . --config Debug --target install
