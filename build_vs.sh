9e6abe3ed6804dc1ddfd7624d05207dc52fcd24e
Csaba Sipos <siposcsaba89@gmail.com>
2018-02-23 19:28:33 +0100

    imgui added

--- build_vs.sh
+++ build_vs.sh
@@ -41,9 +41,9 @@ if [ -z "$AMD_GLES_SDK" ]; then echo "AMD_GLES_SDK is unset"; else echo "AMD_GLE
 
 if [[ ("$1" == "linux") || !(-z "$AMD_GLES_SDK")]]; then
 	glfw_FLAGS="-DGLFW_CLIENT_LIBRARY=glesv2 -DGLFW_USE_EGL=1"
-	projs="glfw zlib freetype assimp"
+	projs="glfw zlib freetype assimp imgui"
 else
-	projs="glfw zlib freetype glew assimp"
+	projs="glfw zlib freetype glew assimp imgui"
 fi
 
 glew_FLAGS=""
